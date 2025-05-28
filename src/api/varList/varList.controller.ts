cef5e1272c591bebdf48032b1cad88fa75dc187f
TurboGLS <matteazzitomas@gmail.com>
2025-05-28 14:46:29 +0200

    var generator with 1 version of data modifier

--- src/api/varList/varList.controller.ts
+++ src/api/varList/varList.controller.ts
@@ -7,17 +7,22 @@ export const downloadVarList = async (
     next: NextFunction
 ) => {
     try {
-        const { type, quantity } = req.query;
+        const { type, quantity, device, ipAddress } = req.body;
 
-        if (!type || !quantity) {
-            res.status(400).json({ message: "Parametri 'type' e 'quantity' obbligatori" });
+        if (!type || !quantity || !device || !ipAddress) {
+            res.status(400).json({ message: "Tutti i Parametri devono essere compilati" });
             return;
         }
 
-        const csv = await generateVarListCSV(type.toString(), parseInt(quantity.toString()));
+        const csv = await generateVarListCSV(
+            type.toString(), 
+            parseInt(quantity.toString()),
+            parseInt(device.toString()),
+            ipAddress.toString()
+        );
 
         res.setHeader('Content-Type', 'text/csv');
-        res.setHeader('Content-Disposition', `attachment; filename=varlist_type${type}_q${quantity}.csv`);
+        res.setHeader('Content-Disposition', `attachment; filename=varlist.csv`);
         res.status(200).send(csv);
     } catch (err) {
         next(err);
--- src/api/varList/varList.service.ts
+++ src/api/varList/varList.service.ts
@@ -1,9 +1,9 @@
 import { SourceModel } from "../sourceMultimetri/source.model";
 import { Parser } from 'json2csv';
 
-export async function generateVarListCSV(type: string, quantity: number) {
+export async function generateVarListCSV(type: string, auxQuantity: number, device: number, ipAddress: string) {
      // prendo i parametri dalla source (varlist)
-     const baseParam = await SourceModel.find({ type }).lean();
+     const baseParam = await SourceModel.find({ type });
 
      if (!baseParam.length) {
         throw new Error(`Parametri varlist non trovati per il tipo ${type}`);
@@ -11,13 +11,16 @@ export async function generateVarListCSV(type: string, quantity: number) {
 
      const varlist: any[] = [];
 
-     for (let i = 1; i <= quantity; i++) {
+     for (let i = 1; i <= auxQuantity; i++) {
         for (const param of baseParam) {
-            const replacedParam = { ...param };
+            const replacedParam = param.toJSON(); // include i virtual
 
             for (const key in replacedParam) {
                 if (typeof replacedParam[key] === 'string') {
-                    replacedParam[key] = replacedParam[key].replace(/§/g, i.toString());
+                    replacedParam[key] = replacedParam[key]
+                        .replace(/§/g, i.toString()) 
+                        .replace(/@/g, device.toString())
+                        .replace(/ç/g, ipAddress.toString())
                 }
             }
             varlist.push(replacedParam);
@@ -26,6 +29,6 @@ export async function generateVarListCSV(type: string, quantity: number) {
      // rimuovere dati inutili
      const cleanedVarlist = varlist.map(({ _id, __v, ...rest }) => rest);
 
-     const parser = new Parser();
+     const parser = new Parser({ quote: '' });
      return parser.parse(cleanedVarlist);
 }
